be2c18dbf491bf3fd6dcfc3e664d1ac7b0eb2917
Samuel Laki <samilaki11@gmail.com>
2025-07-19 03:07:30 -0500

    Update tagline and subtitle

--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -20,16 +20,15 @@ const HeroSection = () => {
             Based In Austin, TX
           </p>
           {/* <h1 className="text-[40px] md:text-6xl lg:text-7xl font-bold text-center max-w-5xl leading-tight tracking-wide">
-            Quality Design &{" "}
-            <span className="text-primary">Web Development</span> Creativity
+            Building AI-Powered Solutions That Actually Work
           </h1> */}
           <TextGenerateEffect
-            words="Quality Design & Web Development Creativity"
+            words="Building AI-Powered Solutions That Actually Work"
             className="text-[40px] md:text-6xl lg:text-7xl font-bold text-center max-w-5xl leading-snug tracking-wide"
           />
           <p className="pt-5 pb-8 text-sm sm:text-base md:text-lg text-dark-200 dark:text-stone-200/70">
-            Hi, I&apos;m Samuel Laki, I create intuitive, visually stunning and
-            highly functional web applications.
+            Hi, I&apos;m Samuel Laki, I create intelligent systems that 
+            transform ideas into powerful, real-world solutions.
           </p>
           <div className="flex flex-col sm:flex-row items-center gap-4">
             <ShinyButton icon={<ChevronRight />}>
